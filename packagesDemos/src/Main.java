cd51e2ccf049ebf5a83ba91cfb77a4d5878c7d20
akgunmustafa <makgun095@gmail.com>
2022-10-05 23:58:36 +0300

    javabootcamp-w4

---
+++ packagesDemos/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+
+        System.out.println("Come on, say your name baby:)");
+        String name=scanner.nextLine();
+        System.out.println("Hello !"+name);
+    }
+}
